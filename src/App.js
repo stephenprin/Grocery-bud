7fea2eb44235b86054d6321e9d8eb1c90ec2049d
stephenprin <stephenprince427@gmail.com>
2022-12-16 23:40:46 +0100

    form to handle the input and display value done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import List from './List';
 import AppCss from './App.module.css';
+import Alert from './components/Alert';
+
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  // const [name, setName] = useState('');
-  // const [list, setList] = useState([]);
-  // const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+   const [isEditing, setIsEditing] = useState(false);
   // const [editID, setEditID] = useState(null);
-  // const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+   const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+      //showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      //show alert
+      const newItem = { id: uuidv4(), title: name };
+      setList([ ...list, newItem ]);
+      console.log(list);
+      setName('');
+    
+    }
+  }
   return (
     <div className={AppCss.sectionCenter}>
+     
+      <form className={AppCss.groceryForm} onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className={AppCss.formControl}>
+          <input type="input" className={AppCss.grocery} placeholder="e.g. eggs" value={name} onChange={(e)=>setName(e.target.value) } />
+          <button type="submit" className={AppCss.submitBtn}>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
       <div className={AppCss.groceryContainer}>
-        <List />
+        <List items={list} />
         <button className={AppCss.clearBtn}>Clear Items</button>
       </div>
     </div>
--- src/List.js
+++ src/List.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 import ListModule from './List.module.css'
 
-const List = () => {
+const List = ({ items }) => {
+    
+
   return (
-    <div className={ListModule.list}>
-        <h2>List Component</h2>
+      <div className={ListModule.groceryList}>
+          {items.map((item) => { 
+              const { id, title } = item;
+              return (
+                <article key={id} className={ListModule.groceryItem}>
+                <p className={ListModule.title}>{title}</p>
+                <div className={ListModule.btnContainer}>
+                    <button type="button" className={ListModule.editBtn}><FaEdit /></button>
+                    <button type="button" className={ListModule.deleteBtn}><FaTrash/></button>
+                </div>
+            </article>
+              )
+          })}
     </div>
   )
 }
